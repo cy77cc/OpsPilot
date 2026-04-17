9abf6aeb0b26bc953daf1d30fc89fb224a80ab87
cy77cc <483564813@qq.com>
2026-04-17 12:32:49 +0800

    fix(server): surface startup errors before worker bootstrap

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,7 +5,9 @@ package server
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net"
 	"net/http"
 	"time"
 
@@ -34,28 +36,65 @@ import (
 // 非阻塞调用，在后台启动服务器。
 // 当 context 被取消时优雅关闭。
 func Start(ctx context.Context) error {
+	if ctx == nil {
+		return errors.New("nil context")
+	}
+
+	started := make(chan struct{})
+	serveErr := make(chan error, 1)
 	done := make(chan struct{})
-	go startServer(ctx, done)
-	<-ctx.Done()
-	logger.L().Info("Shutting Down...........")
-	<-done
-	return nil
+	go startServer(ctx, started, serveErr, done)
+
+	select {
+	case err := <-serveErr:
+		return err
+	case <-started:
+	case <-ctx.Done():
+		select {
+		case err := <-serveErr:
+			return err
+		default:
+			return ctx.Err()
+		}
+	}
+
+	logger.L().Info("http server started", logger.String("addr", fmt.Sprintf("%s:%d", config.CFG.Server.Host, config.CFG.Server.Port)))
+
+	select {
+	case err := <-serveErr:
+		return err
+	case <-ctx.Done():
+		logger.L().Info("Shutting Down...........")
+		<-done
+		select {
+		case err := <-serveErr:
+			return err
+		default:
+			return nil
+		}
+	}
 }
 
 // startServer 启动 Gin 服务。
 //
 // 初始化服务上下文、创建路由、启动监听。
 // 支持优雅关闭，超时时间为 10 秒。
-func startServer(ctx context.Context, done chan struct{}) {
+func startServer(ctx context.Context, started chan struct{}, serveErr chan error, done chan struct{}) {
 	defer close(done)
 
-	svcCtx := svc.MustNewServiceContext()
-	r := NewRouter(ctx, svcCtx)
-
 	srv := &http.Server{
-		Addr:    fmt.Sprintf("%s:%d", config.CFG.Server.Host, config.CFG.Server.Port),
-		Handler: r,
+		Addr: fmt.Sprintf("%s:%d", config.CFG.Server.Host, config.CFG.Server.Port),
 	}
+
+	listener, err := net.Listen("tcp", srv.Addr)
+	if err != nil {
+		serveErr <- err
+		return
+	}
+	defer listener.Close()
+
+	svcCtx := svc.MustNewServiceContext()
+	srv.Handler = NewRouter(ctx, svcCtx)
 
 	go func() {
 		<-ctx.Done()
@@ -68,10 +107,11 @@ func startServer(ctx context.Context, done chan struct{}) {
 			logger.L().Error("http shutdown error", logger.Error(err))
 		}
 	}()
-	logger.L().Info("http server started", logger.String("addr", srv.Addr))
+
+	close(started)
 
 	// 阻塞监听
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		return
+	if err := srv.Serve(listener); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		serveErr <- err
 	}
 }
