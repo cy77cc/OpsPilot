b9c9d50bd831577e612f1ccef0960726ab337959
cy77cc <483564813@qq.com>
2026-04-21 16:04:20 +0800

    cleanup: remove unused FieldGuideCard component

--- web/src/components/FormGuidance/index.ts
+++ web/src/components/FormGuidance/index.ts
@@ -1,4 +1,3 @@
 export type { FieldGuide, GuidedFormItemProps } from './types';
-export { default as FieldGuideCard } from './FieldGuideCard';
 export { default as GuidedFormItem } from './GuidedFormItem';
 export { default as AIFormAssistantPopover } from './AIFormAssistantPopover';
